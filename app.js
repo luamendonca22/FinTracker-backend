e594aa01c30aaaf952028dc2234873c0cc835755
luamendonca21 <luamendonca_2000@hotmail.com>
2023-02-25 19:29:47 +0000

    login route and private route when user try to access a user id without a token

--- app.js
+++ app.js
@@ -11,13 +11,45 @@ app.use(express.json());
 
 // Models
 const User = require("./models/User");
+
 // Open Route - Public Route
 app.get("/", (req, res) => {
   res.status(200).json({ msg: "Bem vindo à nossa API!" });
 });
 
-// Register User
+// Private Route
+app.get("/user/:id", checkToken, async (req, res) => {
+  const id = req.params.id;
 
+  // check if user exists
+  const user = await User.findById(id, "-password");
+  if (!user) {
+    return res.status(404).json({ msg: "O utilizador não foi encontrado" });
+  }
+  return res.status(200).json({ user });
+});
+
+function checkToken(req, res, next) {
+  // token
+  const authHeader = req.headers["authorization"];
+
+  // vem neste formato "Bearer safsajdhae", temos que fazer split
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({ msg: "Acesso negado" });
+  }
+
+  try {
+    const secret = process.env.SECRET;
+    jwt.verify(token, secret);
+    next();
+  } catch (error) {
+    res.status(400).json({ msg: "Token inválido" });
+  }
+}
+
+// Register User
 // async -> some things will depend of some response time
 app.post("/auth/register", async (req, res) => {
   const { name, email, password } = req.body;
@@ -48,7 +80,7 @@ app.post("/auth/register", async (req, res) => {
 
   try {
     await user.save();
-    res.status(201).json({ msg: "Usuário criado com sucesso!" });
+    res.status(201).json({ msg: "Utilizador criado com sucesso!" });
   } catch (error) {
     console.log(error);
     res.status(500).json({
@@ -57,6 +89,44 @@ app.post("/auth/register", async (req, res) => {
   }
 });
 
+// Login User
+app.post("/auth/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  //vvalidations
+  if (!email) {
+    return res.status(422).json({ msg: "O email é obrigatório" });
+  }
+  if (!password) {
+    return res.status(422).json({ msg: "A palavra-passe é obrigatório" });
+  }
+
+  // check if user exists
+  const user = await User.findOne({ email: email });
+  if (!user) {
+    return res.status(404).json({ msg: "O utilizador não foi encontrado" });
+  }
+
+  // check if password match
+  const checkPassword = await bcrypt.compare(password, user.password);
+
+  if (!checkPassword) {
+    return res.status(422).json({ msg: "Senha inválida" });
+  }
+
+  try {
+    const secret = process.env.SECRET;
+    const token = jwt.sign({ id: user._id }, secret);
+    res
+      .status(200)
+      .json({ msg: "Login realizado com sucesso", token, id: user._id });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      msg: "Ocorreu um erro no servidor, tente novamente mais tarde.",
+    });
+  }
+});
 const dbUser = process.env.DB_USER;
 const dbPassword = process.env.DB_PASS;
 
