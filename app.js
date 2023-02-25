e9b97ac987b333f2c6617e880941a59a7b223176
luamendonca21 <luamendonca_2000@hotmail.com>
2023-02-25 17:20:23 +0000

    connection with database

--- app.js
+++ app.js
@@ -1,2 +1,26 @@
 require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const app = express();
+
+// Open Route - Public Route
+app.get("/", (req, res) => {
+  res.status(200).json({ msg: "Bem vindo à nossa API!" });
+});
+
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASS;
+
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(
+    `mongodb+srv://${dbUser}:${dbPassword}@cluster0.4unhbpa.mongodb.net/?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    app.listen(3000);
+    console.log("Conection sucessfully");
+  })
+  .catch((err) => console.log(err));
