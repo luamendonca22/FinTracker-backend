f15fb196d23e6ae3d1424824503e312848b07dcd
luamendonca21 <luamendonca_2000@hotmail.com>
2023-06-28 19:16:46 +0100

    comment now has timestamps

--- models/Comment.js
+++ models/Comment.js
@@ -1,8 +1,11 @@
 const mongoose = require("mongoose");
 
-const CommentSchema = new mongoose.Schema({
-  text: { type: String, required: true },
-  userId: { type: String, required: true },
-});
+const CommentSchema = new mongoose.Schema(
+  {
+    text: { type: String, required: true },
+    userId: { type: String, required: true },
+  },
+  { timestamps: { createdAt: true, updatedAt: false } }
+);
 const Comment = mongoose.model("Comment", CommentSchema);
 module.exports = { Comment, CommentSchema };
